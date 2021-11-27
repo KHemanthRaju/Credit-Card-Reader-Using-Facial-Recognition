08379f76e6c1d7b2958dd045020e6d22dd1a2f58
Koneti Hemanth Raju <49878564+KHemanthRaju@users.noreply.github.com>
2021-11-27 17:52:18 +0530

    Updated (#2)

---
+++ capture.py
@@ -0,0 +1,50 @@
+import cv2
+from time import sleep
+
+# Method to capture face of a person
+def capt():
+    f1=open("Accnt_Record.txt",'r')
+    accnt_no=int(f1.readline())
+    accnt_no+=1
+    f1.close()
+    key = cv2. waitKey(1)
+    webcam = cv2.VideoCapture(0)
+    sleep(2)
+    while True:
+
+        try:
+            check, frame = webcam.read()
+            print(check) #prints true as long as the webcam is running
+            print(frame) #prints matrix values of each frame
+            cv2.imshow("Capturing", frame)
+            key = cv2.waitKey(1)
+
+            if key == ord('s'): 
+                cv2.imwrite(filename=str(accnt_no)+'.jpg', img=frame)
+                webcam.release()
+                print("Processing image...")
+                img_ = cv2.imread(str(accnt_no)+'.jpg', cv2.IMREAD_ANYCOLOR)
+                print("Converting RGB image to grayscale...")
+                gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
+                print("Converted RGB image to grayscale...")
+                print("Resizing image to 28x28 scale...")
+                img_ = cv2.resize(gray,(28,28))
+                print("Resized...")
+                img_resized = cv2.imwrite(filename=str(accnt_no)+'blac.jpg', img=img_)
+                print("Image saved!")
+                cv2.destroyAllWindows()
+            
+                break
+        
+            elif key == ord('q'):
+                webcam.release()
+                cv2.destroyAllWindows()
+                break
+    
+        except(KeyboardInterrupt):
+            print("Turning off camera.")
+            webcam.release()
+            print("Camera off.")
+            print("Program ended.")
+            cv2.destroyAllWindows()
+            break
